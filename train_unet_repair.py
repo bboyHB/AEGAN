3d0ed16267b637e53a336e5f5dc79cb5fc6cf3ee
bboyHB-MOCOM <245908564@qq.com>
2021-01-23 19:35:09 +0800

    11

--- train_unet_repair.py
+++ train_unet_repair.py
@@ -15,7 +15,7 @@ from torchvision.transforms import functional as F
 
 from config import config
 from FocalLoss2d import FocalLossG
-from unet_gan.unet_gan import UNetGAN
+from unet_gan.unet_gan import UNetGenerator, UNetDiscriminator
 from defect_generate_gan.dg_gan import add_defect
 
 
@@ -27,12 +27,13 @@ iters_per_epoch = 1000
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 def Unet_repair_autodata():
-    ug = UNetGAN(bilinear=True).to(device)
+    G = UNetGenerator().to(device)
+    D = UNetDiscriminator().to(device)
     criterion_G = nn.MSELoss()
     criterion_D = nn.BCELoss()
 
-    optimizer_G = SGD(ug.netG.parameters(), lr=config.lr, momentum=0.9, weight_decay=1e-8)
-    optimizer_D = SGD(ug.netD.parameters(), lr=config.lr, momentum=0.9, weight_decay=1e-8)
+    optimizer_G = SGD(G.parameters(), lr=config.lr, momentum=0.9, weight_decay=1e-8)
+    optimizer_D = SGD(D.parameters(), lr=config.lr, momentum=0.9, weight_decay=1e-8)
     step_lr_G = StepLR(optimizer_G, config.lr_step, config.lr_decay_gamma)
     step_lr_D = StepLR(optimizer_D, config.lr_step, config.lr_decay_gamma)
 
@@ -64,26 +65,29 @@ def Unet_repair_autodata():
             gt_real_soft = torch.tensor(np.random.uniform(0.0, 0.3, (img_origin_tensor.shape[0], 1)), dtype=torch.float).to(device)
             target_recon_soft = torch.tensor(np.random.uniform(0.0, 0.3, (img_origin_tensor.shape[0], 1)), dtype=torch.float).to(device)
 
-            img_G, judge_fake, judge_real = ug(noise_img_defect_tensor, img_origin_tensor)
-
+            img_G = G(noise_img_defect_tensor)
             recon_loss = criterion_G(img_G, img_origin_tensor)
-            lie_loss = criterion_D(judge_fake, target_recon_soft)
-
-            detect_loss = criterion_D(judge_fake, gt_recon_soft)
-            real_loss = criterion_D(judge_real, gt_real_soft)
-
             optimizer_G.zero_grad()
             recon_loss.backward()
             optimizer_G.step()
 
+            pair_fake = torch.cat((img_G, noise_img_defect_tensor), dim=1)
+            judge_fake = D(pair_fake)
+            lie_loss = criterion_D(judge_fake.squeeze(0).squeeze(0), target_recon_soft)
             optimizer_G.zero_grad()
             lie_loss.backward()
             optimizer_G.step()
 
+            pair_fake = torch.cat((img_G.detach(), noise_img_defect_tensor), dim=1)
+            judge_fake = D(pair_fake)
+            detect_loss = criterion_D(judge_fake, gt_recon_soft)
             optimizer_D.zero_grad()
             detect_loss.backward()
             optimizer_D.step()
 
+            pair_real = torch.cat((img_origin_tensor, noise_img_defect_tensor), dim=1)
+            judge_real = D(pair_real)
+            real_loss = criterion_D(judge_real, gt_real_soft)
             optimizer_D.zero_grad()
             real_loss.backward()
             optimizer_D.step()
